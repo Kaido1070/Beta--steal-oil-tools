1b9fde19e5fe139173ae9662327da6a60014db98
1Yono <f8h0f8@gmail.com>
2026-09-01 08:34:18 +0300

    Load preset visuals in v5.47

--- js/layout-quick-compare.js
+++ js/layout-quick-compare.js
@@ -1,6 +1,6 @@
 (() => {
-  const VERSION = "5.46";
-  const styles = ["css/v539-01.css", "css/v539-02.css", "css/v539-03.css", "css/beta-first-visit.css", "css/beta-image-atlas-fix.css"];
+  const VERSION = "5.47";
+  const styles = ["css/v539-01.css", "css/v539-02.css", "css/v539-03.css", "css/beta-first-visit.css", "css/beta-image-atlas-fix.css", "css/beta-preset-visuals.css"];
   for (const href of styles) {
     const link = document.createElement("link");
     link.rel = "stylesheet";
@@ -13,7 +13,8 @@
     "js/v539-11.js",
     "js/beta-oil-order.js",
     "js/beta-first-visit.js",
-    "js/beta-image-atlas-fix.js"
+    "js/beta-image-atlas-fix.js",
+    "js/beta-preset-visuals.js"
   ];
   let index = 0;
   const loadNext = () => {
